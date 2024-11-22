d224cbb66e4703d23a7c18ea14906e8c0d9dd455
Jude Rose <jurosecpt024@student.wethinkcode.co.za>
2024-11-22 08:21:13 +0200

    Enemy and player skeletons added

--- classes.py
+++ classes.py
@@ -2,5 +2,37 @@ import pygame
 import pygame.locals
 
 class Enemy:
-    def __init__(self) -> None:
+    def __init__(self,type,image) -> None:
+        self.image = pygame.image.load(image).convert()
+        self.type = type
+    
+    def behavior(self):
         pass
+        #if self.type == whatever type
+            #run func
+        #elif...
+
+    #def whatevertype(self):
+        #set behavior
+
+class Player:
+    def __init__(self) -> None:
+        #self.image = pygame.image.load('''image''').convert()
+        pass
+
+    def movement(self):
+        pass
+
+    def collision(self):
+        pass
+
+    def death(self):
+        pass
+
+    #def powerup1(self):
+        #set behavior
+    
+    #def powerup2(self):
+        #set behavior
+    
+    #def ...
